1a985c8b7bd3282e9d97c7e47c5e1636a1de0f32
Anil Wadghule <anildigital@gmail.com>
2015-02-14 10:43:19 +0530

    Function

--- first.rs
+++ first.rs
@@ -1,6 +1,7 @@
 fn main() {
-    let i = 1i32;
-    let j: i32 = 1;
-    let y = true;
-    println!("y: {}, i: {}, j: {}", y, i, j);
+    foo(3);
+}
+
+fn foo(x: i32) -> bool {
+    return (x > 1);
 }
